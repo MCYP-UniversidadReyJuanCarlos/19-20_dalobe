f541c4840c285e2af73fef27f2df1ff5cc1a25c0
d.lopezb <d.lopezb.2019@alumnos.urjc.es>
2020-06-23 18:17:41 +0200

    Create new endpoint to get containers, changelog, api and source code

--- project/resource/check_container_resource.py
+++ project/resource/check_container_resource.py
@@ -1,28 +1,30 @@
+import json
+
 from flask import Flask, jsonify
 
+from project.resource.payload.container_resource_payload import ContainerPayload
 from project.service.check_container_service import Check
+from project.service.containers_service import ContainerService
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return send_response()
+@app.route("/health")
+def health():
+    return jsonify({"status": "UP"}), 200
+
+
+@app.route('/containers')
+def get_containers():
+    containers_payload = [ContainerPayload(v) for v in ContainerService().get_containers()]
+    json_containers = [o.json() for o in containers_payload]
+    return jsonify({"result": json_containers}), 200
 
 
 @app.route('/containers/<id>/check')
-def check(id):
+def check_container(container_id):
     check = Check()
-    return jsonify({"evaluation": check.check_and_fix(id)}), 200
-
-
-@app.route("/health")
-def health():
-    return jsonify({"message": "Running..."}), 200
-
-
-def send_response():
-    return jsonify({"message": "Running..."}), 200
+    return jsonify({"result": check.check_and_fix(container_id)}), 200
 
 
 if __name__ == '__main__':
