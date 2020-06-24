d966a4294de2b8c32b09dafecb1d5249c6e161a9
d.lopezb <d.lopezb.2019@alumnos.urjc.es>
2020-06-24 18:17:58 +0200

    Add unit test for container service

--- project/resource/check_container_resource.py
+++ project/resource/check_container_resource.py
@@ -1,6 +1,7 @@
 import json
 
 from flask import Flask, jsonify
+from flask_cors import cross_origin
 
 from project.resource.payload.container_resource_payload import ContainerPayload
 from project.service.check_container_service import Check
@@ -9,19 +10,22 @@ from project.service.containers_service import ContainerService
 app = Flask(__name__)
 
 
-@app.route("/health")
+@app.route("/sds/health")
+@cross_origin()
 def health():
     return jsonify({"status": "UP"}), 200
 
 
-@app.route('/containers')
+@app.route('/sds/containers')
+@cross_origin()
 def get_containers():
     containers_payload = [ContainerPayload(v) for v in ContainerService().get_containers()]
     json_containers = [o.json() for o in containers_payload]
-    return jsonify({"result": json_containers}), 200
+    return jsonify(json_containers), 200
 
 
-@app.route('/containers/<id>/check')
+@app.route('/sds/containers/<id>/check')
+@cross_origin()
 def check_container(container_id):
     check = Check()
     return jsonify({"result": check.check_and_fix(container_id)}), 200
