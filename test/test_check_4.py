0aee85ba93082002fda04c27724c16a2baaf140f
d.lopezb <d.lopezb.2019@alumnos.urjc.es>
2020-06-27 09:16:51 +0200

    Fix error in unit test

--- test/test_check_4.py
+++ test/test_check_4.py
@@ -24,14 +24,14 @@ class Check_4_1_Test(unittest.TestCase):
         container = Container()
         ret = Check_4_1.evaluate_container(container)
         assert mocked_inspect.called
-        self.assertDictEqual(ret, {'result': 'OK'})
+        self.assertDictEqual(ret, {'evaluation': 'OK'})
 
     @mock.patch("docker.APIClient.inspect_container", return_value=inspect_container_root_user_item)
     def test_given_container_when_config_user_is_root_then_KO(self, mocked_inspect):
         container = Container()
         ret = Check_4_1.evaluate_container(container)
         assert mocked_inspect.called
-        self.assertDictEqual({'result': 'KO', 'code': 'CONTAINER_RUNNING_AS_ROOT',
+        self.assertDictEqual({'evaluation': 'KO', 'code': 'CONTAINER_RUNNING_AS_ROOT',
                               'description': 'It is a good practice to run the container as a non-root '
                                              + 'user, if possible.'}, ret)
 
@@ -40,7 +40,7 @@ class Check_4_1_Test(unittest.TestCase):
         container = Container()
         ret = Check_4_1.evaluate_container(container)
         assert mocked_inspect.called
-        self.assertDictEqual({'result': 'KO', 'code': 'NO_USER_FOUND_RUNNING_CONTAINER',
+        self.assertDictEqual({'evaluation': 'KO', 'code': 'NO_USER_FOUND_RUNNING_CONTAINER',
                               'description': 'It is a good practice to run the container as a non-root '
                                              + 'user, if possible.'}, ret)
 
@@ -49,7 +49,7 @@ class Check_4_1_Test(unittest.TestCase):
         container = Container()
         ret = Check_4_1.evaluate_container(container)
         assert mocked_inspect.called
-        self.assertDictEqual(ret, {'result': 'KO', 'code': 'NO_USER_FOUND_RUNNING_CONTAINER',
+        self.assertDictEqual(ret, {'evaluation': 'KO', 'code': 'NO_USER_FOUND_RUNNING_CONTAINER',
                                    'description': 'It is a good practice to run the container as a non-root '
                                                   + 'user, if possible.'})
 
@@ -71,7 +71,7 @@ class Check_4_6_Test(unittest.TestCase):
         container = Container()
         ret = Check_4_6.evaluate_container(container)
         assert mocked_inspect.called
-        self.assertDictEqual(ret, {'result': 'KO',
+        self.assertDictEqual(ret, {'evaluation': 'KO',
                                    'code': 'NO_HEALTHCHECK_CONFIGURED',
                                    'description': 'You should add the HEALTHCHECK instruction to your Docker '
                                                   'container images in '
