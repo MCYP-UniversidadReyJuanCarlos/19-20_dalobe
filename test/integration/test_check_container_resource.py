dfbd0c85bf5f687c0423990372f9b52fb784af74
d.lopezb <d.lopezb.2019@alumnos.urjc.es>
2020-07-13 17:21:25 +0200

    Fix error in path

--- test/integration/test_check_container_resource.py
+++ test/integration/test_check_container_resource.py
@@ -15,28 +15,28 @@ class Test(TestCase):
 
     def test_post_given_dockerfile_with_user_when_check_then_OK(self):
         result = self.app.post('/sds/images/dockerfile/check', json={
-            'dockerFile': '../resources/Dockerfile_with_user'})
+            'dockerFile': 'test/resources/Dockerfile_with_user'})
         data = json.loads(result.data)
         user_check_result = list(filter(lambda x: x['4_1']['evaluation'] == 'OK', data["dockerFile"]))
         self.assertEqual(len(user_check_result),1)
 
     def test_post_given_dockerfile_without_user_when_check_then_KO(self):
         result = self.app.post('/sds/images/dockerfile/check', json={
-            'dockerFile': '../resources/Dockerfile_test1'})
+            'dockerFile': 'test/resources/Dockerfile_test1'})
         data = json.loads(result.data)
         user_check_result = list(filter(lambda x: x['4_1']['evaluation'] == 'KO', data["dockerFile"]))
         self.assertEqual(len(user_check_result),1)
 
     def test_post_given_dockerfile_with_healthcheck_when_check_then_OK(self):
         result = self.app.post('/sds/images/dockerfile/check', json={
-            'dockerFile': '../resources/Dockerfile_with_user'})
+            'dockerFile': 'test/resources/Dockerfile_with_user'})
         data = json.loads(result.data)
         user_check_result = list(filter(lambda x: x['4_1']['evaluation'] == 'OK', data["dockerFile"]))
         self.assertEqual(len(user_check_result),1)
 
     def test_post_given_dockerfile_without_healthcheck_when_check_then_KO(self):
         result = self.app.post('/sds/images/dockerfile/check', json={
-            'dockerFile': '../resources/Dockerfile_test1'})
+            'dockerFile': 'test/resources/Dockerfile_test1'})
         data = json.loads(result.data)
         user_check_result = list(filter(lambda x: x['4_6']['evaluation'] == 'KO', data["dockerFile"]))
         self.assertEqual(len(user_check_result),1)
